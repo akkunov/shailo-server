e3763ead0d9de1c71971dc49072588a4329a9e90
Elpacho <akunov313131@gmail.com>
2025-10-08 19:18:21 +0600

    первый комит

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
+console.log("ENV:", process.env.DATABASE_URL);
 
 app.listen(PORT,"0.0.0.0", () => {
     console.log(`🚀 Server running on :${PORT}`);
